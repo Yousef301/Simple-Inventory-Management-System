ece70ef51a54067b482b27235ed335e80f1677e5
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-02-29 12:14:07 +0200

    Add Inventory class with AddProduct method.

---
+++ SimpleInventoryManagementSystem/Inventory.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleInventoryManagementSystem;
+
+public class Inventory
+{
+    private List<Product> _products = new();
+
+    public List<Product> Products
+    {
+        get { return _products; }
+    }
+
+    public void AddProduct(Product product)
+    {
+        var matches = _products.Where(p => String.Equals(p.Name, product.Name, StringComparison.CurrentCulture));
+        if (matches.Any()) Console.WriteLine($"{product.Name} is already in the inventory.");
+        else
+        {
+            _products.Add(product);
+            Console.WriteLine($"{product.Name} is added successfully.");
+        }
+    }
+}
