b7969b69394d18349a564e0dd393f02870113c05
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-02-29 11:54:41 +0200

    Add Product class with its attributes and functionality.

---
+++ SimpleInventoryManagementSystem/Product.cs
@@ -0,0 +1,45 @@
+﻿namespace SimpleInventoryManagementSystem;
+
+public class Product
+{
+    private string name;
+    private double price;
+    private int quantity;
+
+    public string Name
+    {
+        get => name;
+        set => name = value.Length > 30 ? value[..30] : value;
+    }
+
+    public double Price
+    {
+        get => price;
+        set => price = value > 0 ? value : price;
+    }
+
+    public int Quantity
+    {
+        get => quantity;
+        set => quantity = value > 0 ? value : quantity;
+    }
+
+    public Product(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public void IncreaseQuantity(int amount)
+    {
+        if (amount > 0) Quantity += amount;
+        else Console.WriteLine($"Couldn't add {amount}");
+    }
+
+    public void DecreaseQuantity(int amount)
+    {
+        if (Quantity - amount > 0) Quantity += amount;
+        else Console.WriteLine($"Can't remove {amount}. Available quantity is {Quantity}");
+    }
+}
--- SimpleInventoryManagementSystem/Program.cs
+++ SimpleInventoryManagementSystem/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace SimpleInventoryManagementSystem;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
